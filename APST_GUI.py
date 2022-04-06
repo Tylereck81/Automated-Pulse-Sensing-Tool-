41e5df1858bed41ec2489d253042f17cac1b5aee
Tyler <tylereck81@yahoo.com>
2022-04-07 05:45:18 +0800

    back to it

--- APST_GUI.py
+++ APST_GUI.py
@@ -18,14 +18,12 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, 
 NavigationToolbar2Tk)
-import numpy as np 
 
 
 
 current_pos = [0,0,0]
 global Move
 Move =""
-global Step
 Step = 10
 old_pos =[0,0,0]
 MAX_X = 250
@@ -42,9 +40,6 @@ global arduino_port
 arduino_port = 'COM3'
 global READING
 READING = 0
-
-Pressure_graph  =[] 
-Pulse_graph = []
 
 Measure = { 
     "X":[], 
@@ -128,7 +123,7 @@ def arduino_move():
             old_pos = deepcopy(current_pos)
             print(num)
             arduino.write(num.encode())
-            # time.sleep(0.5)
+            #time.sleep(0.5)
             # data = arduino.readline()
             # print(data)
 
@@ -146,7 +141,6 @@ def start_scan():
 
     plot_sensor_data()
 
-
 def stop_scan(): 
 
     #Set stop flag to true
@@ -156,9 +150,6 @@ def stop_scan():
     Measure["X"]=[]
     Measure["Pressure"] =[] 
     Measure["Pulse"] = []
-
-    Pressure_graph = [] 
-    Pulse_graph = []
 
 
 
@@ -193,22 +184,10 @@ def sensor_read():
         if len(n) == 9:
 
             Pressure = (n[5]<<8)|n[4]
-            Pulse = int(((n[7]<<8)|n[6])/25)
+            Pulse = (n[7]<<8)|n[6]
             Measure["X"].append(X) 
             Measure["Pressure"].append(Pressure)
-            Measure["Pulse"].append(Pulse)  
-
-            if len(Pressure_graph) < 100: 
-                Pressure_graph.append(Pressure) 
-            else: 
-                Pressure_graph[0:99] = Pressure_graph[1:100] 
-                Pressure_graph[99] = Pressure
-            
-            if len(Pulse_graph) < 100: 
-                Pulse_graph.append(Pulse) 
-            else: 
-                Pulse_graph[0:99] = Pulse_graph[1:100] 
-                Pulse_graph[99] = Pulse
+            Measure["Pulse"].append(Pulse)
             X+=1
             n=[]        
         if STOP_SCAN:
@@ -226,16 +205,14 @@ def sensor_read():
 def plot_sensor_data(): 
 
     def animate(i):
-        x1 = np.arange(0,len(Pressure_graph))
-        x2 = np.arange(0,len(Pulse_graph))
-
-        Pressure = Pressure_graph
-        Pulse = Pulse_graph
+        x = Measure["X"]
+        Pressure = Measure["Pressure"] 
+        Pulse = Measure["Pulse"]
 
         plt.cla()
 
-        plt.plot(x2, Pulse, label ='Pulse')
-        plt.plot(x1, Pressure, label ='Pressure')
+        plt.plot(x, Pulse, label ='Pulse')
+        plt.plot(x, Pressure, label ='Pressure')
 
         plt.legend(loc = "upper left")
         plt.tight_layout()
@@ -243,6 +220,8 @@ def plot_sensor_data():
     ani = FuncAnimation(plt.gcf(),animate, interval=1)
     plt.show()
 
+        
+        
 
 
 
@@ -312,18 +291,6 @@ def Manual():
     Left["state"] = "normal"
     Right["state"] = "normal"
 
-def Set_Step():
-    global Step
-    IN = step_to_move.get("1.0","end-1c")
-    Step = int(IN)
-
-l = Label(text = "Step to Move") 
-step_to_move = Text(root, height = 2, width = 2)
-set_step = Button(root, text = "set", padx =10, pady = 10, command = Set_Step)
-
-l.grid(row = 1, column = 9) 
-step_to_move.grid(row = 2, column = 9)
-set_step.grid(row = 2, column = 10)
 
 #Buttons for Automatic or Manual mode 
 Automatic_Button = Button(root, text = "Automatic",padx = 10, pady = 10, command = Automatic)
@@ -368,8 +335,8 @@ Arduino_Connect_Status.grid(row = 7, column = 2)
 
 
 def main(): 
-    t1 = Thread(target = arduino_move, daemon=True)
-    t1.start()
+    # t1 = Thread(target = arduino_move, daemon=True)
+    # t1.start()
     t2 = Thread(target = connect_sensor, daemon = True)
     t2.start() 
 
